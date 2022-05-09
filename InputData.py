106f1f06d6a652c72f671a385cbcc93108b5952c
daisyruthb <daisy.bourne@yale.edu>
2022-05-09 11:45:34 -0400

    Daisy added files from Lab 10

--- InputData.py
+++ InputData.py
@@ -6,43 +6,55 @@ SIM_TIME_STEPS = 100    # length of simulation (years)
 ALPHA = 0.05        # significance level for calculating confidence intervals
 DISCOUNT = 0.03     # annual discount rate
 
-
 class HealthStates(Enum):
     """ health states of patients with HIV """
-    CD4_200to500 = 0
-    CD4_200 = 1
-    AIDS = 2
-    HIV_DEATH = 3
-
+    WELL = 0
+    HR_HPV = 1
+    L_DYSPLASIA = 2
+    H_DYSPLASIA = 3
+    CANCER = 4
+    CANCER_DEATH = 5
+    OTHER_DEATH = 6
 
 # transition matrix
 TRANS_MATRIX = [
-    [1251,  350,    116,    17],   # CD4_200to500
-    [0,     731,    512,    15],   # CD4_200
-    [0,     0,      1312,   437]   # AIDS
+    [0, 0, 0, 0, 0, 0, 0],   # WELL
+    [0, 0, 0, 0, 0, 0, 0],   # HR_HPV
+    [0, 0, 0, 0, 0, 0, 0],   # L_DYSPLASIA
+    [0, 0, 0, 0, 0, 0, 0],   # H_DYSPLASIA
+    [0, 0, 0, 0, 0, 0, 0],   # CANCER
+    [0, 0, 0, 0, 0, 0, 0],   # CANCER_DEATH
+    [0, 0, 0, 0, 0, 0, 0]   # OTHER_DEATH
     ]
 
 # annual cost of each health state
 ANNUAL_STATE_COST = [
-    2756.0,     # CD4_200to500
-    3025.0,     # CD4_200
-    9007.0,     # AIDS
-    0,          # Dead
+    0,     # WELL
+    0,     # HR_HPV
+    0,     # L_DYSPLASIA
+    0,     # H_DYSPLASIA
+    1702,  # CANCER
+    0,     # CANCER_DEATH
+    0,     # OTHER_DEATH
     ]
 
 # annual health utility of each health state
-ANNUAL_STATE_UTILITY = [
-    0.75,   # CD4_200to500
-    0.50,   # CD4_200
-    0.25,   # AIDS
-    0,      # Dead
-    ]
+# ANNUAL_STATE_UTILITY = [
+#     0,  # WELL
+#     0,  # HR_HPV
+#     0,  # L_DYSPLASIA
+#     0,  # H_DYSPLASIA
+#     1702,  # CANCER
+#     0,  # CANCER_DEATH
+#     0,  # OTHER_DEATH
+#     ]
 
-# annual drug costs
-Zidovudine_COST = 2278.0
-Lamivudine_COST = 2086.0
+# treatment costs
+HPV_VACC_COST = 400
+HPV_SCREENING = 117
+CRYT_SCREENING = 98
+DUAL_SCREENING = 142
+COLPOSCOPY_COST = 299
 
 # treatment relative risk
 TREATMENT_RR = 0.509
-
-
