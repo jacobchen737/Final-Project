bbc1a16f36e5fab5e0783e2e25e07e6c16c553f6
JacobChen <jacob.chen.gc643@yale.edu>
2022-05-10 09:30:58 -0400

    added matrix

--- InputData.py
+++ InputData.py
@@ -19,10 +19,56 @@ class HealthStates(Enum):
     CANCER_DEATH = 7
     OTHER_DEATH = 8
 
-# treatment sensisitivy
+# treatment sensitivity
 HPV_SCREEN_RR = 0.9260
 CRYT_SCREEN_RR = 0.7550
 DUAL_SCREEN_RR = 0.9370
+
+ANNUAL_PROB_BACKGROUND_MORT = 41.4/1000
+ANNUAL_PROB_CERVICALCANCER_MORT = 36.2/100000
+ANNUAL_PROB_FIRST_CERVICALCANCER = 15/1000
+PROB_SCREEN_WELL = 0.75
+PROB_SCREEN_DISEASE = 0.7
+FIVE_YEAR_PROB_RECURRENT_CANCER = 0.17
+SCREEN_DURATION = 1/365
+
+# Part 1: find the annual probability of non-cervical-cancer death
+annual_prob_non_cervicalcancer_mort = (ANNUAL_PROB_ALL_CAUSE_MORT - ANNUAL_PROB_CERVICALCANCER_MORT)
+lambda0 = -np.log(1 - annual_prob_non_cervicalcancer_mort)
+
+# Part 2: lambda 1 + lambda 2
+lambda1_plus2 = -np.log(1 - ANNUAL_PROB_FIRST_CERVICALCANCER)
+
+# Part 3
+lambda1 = lambda1_plus2 * PROB_SURVIVE_FIRST_STROKE
+lambda2 = lambda1_plus2 * (1 - PROB_SURVIVE_FIRST_STROKE)
+
+# Part 4
+lambda3_plus4 = -1 / 5 * np.log(1 - FIVE_YEAR_PROB_RECURRENT_STROKE)
+
+# Part 5
+lambda3 = lambda3_plus4 * PROB_SURVIVE_RECURRENT_STROKE
+lambda4 = lambda3_plus4 * (1 - PROB_SURVIVE_RECURRENT_STROKE)
+
+# Part 6
+lambda5 = 1 / SCREEN_DURATION
+
+# find multipliers to adjust the rates out of "Post-Stroke" depending on whether the patient
+# is receiving anticoagulation or not
+if with_treatment:
+    r1 = 1 - ANTICOAG_STROKE_REDUCTION
+    r2 = 1 + ANTICOAG_BLEEDING_DEATH_INCREASE
+else:
+    r1 = 1
+    r2 = 1
+
+rate_matrix = [
+    [0, lambda1, 0, lambda2, lambda0],  # WELL
+    [0, 0, lambda5, 0, 0],  # STROKE
+    [0, lambda3 * r1, 0, lambda4 * r1, lambda0 * r2],  # POST-STROKE
+    [0, 0, 0, 0, 0],  # STROKE-DEATH
+    [0, 0, 0, 0, 0]  # NATURAL-DEATH
+]
 
 TRANS_MATRIX_HPV = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],   # WELL
